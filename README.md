2693c306741bf4a18d0eaa04242cf4f6bc5fc37d
JP Hwang <me@jphwang.com>
2020-09-06 21:37:29 +0800

    For data exploration article

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Learn data science with sports data
+# Sports data science projects
+Interesting, relatable projects for learning data science with sports data from the NBA.  
+## Build a re-usable interactive data exploration app
+Learn how to exploratory analysis of NBA data with leading modern data science tools Plotly and Streamlit.
 
-## Data exploration
-Build a streamlit app to dynamically explore data
-
-
-
+- Article Link: TBD
+- Script: data_explorer_app.py
+- Data: player_per_game.csv
--- data_explorer_app.py
+++ data_explorer_app.py
@@ -12,7 +12,6 @@ sh.setFormatter(formatter)
 root_logger.addHandler(sh)
 
 import pandas as pd
-import numpy as np
 import streamlit as st
 import plotly.express as px
 
@@ -23,13 +22,17 @@ df = df.assign(pos_simple=df.pos.apply(lambda x: x.split("-")[0]))
 st.title("Data exploration demo app - NBA data")
 st.write(f"Data from {df.season.min()} to {df.season.max()} seasons.")
 
+st.subheader("Raw Dataset")
+st.write(df)
+st.write(df.describe())
 # For univariate distributions
 # histogram to better understand
 st.header("Histogram")
 hist_x = st.selectbox("Histogram variable", options=df.columns, index=df.columns.get_loc("mp_per_g"))
-hist_bins = int(st.slider(label="Histogram bins", min_value=5, max_value=50, value=25, step=1))
+hist_bins = st.slider(label="Histogram bins", min_value=5, max_value=50, value=25, step=1)
+hist_cats = df[hist_x].sort_values().unique()
 hist_fig = px.histogram(df, x=hist_x, nbins=hist_bins, title="Histogram of " + hist_x,
-                        template="plotly_white")
+                        template="plotly_white", category_orders={hist_x: hist_cats})
 st.write(hist_fig)
 
 # boxplots
@@ -37,7 +40,7 @@ st.header("Boxplot")
 st.subheader("With a categorical variable - position, age or season")
 box_x = st.selectbox("Boxplot variable", options=df.columns, index=df.columns.get_loc("pts_per_g"))
 box_cat = st.selectbox("Categorical variable", ["pos_simple", "age", "season"], 0)
-st.write("Hint - try 3 point attempts per game ('fg3a_per_g') as a function of season, or points per game ('pts_per_g') as a function of age.")
+st.write("Hint - try correlating 3 point attempts per game ('fg3a_per_g') with season, or points per game ('pts_per_g') with age.")
 box_fig = px.box(df, x=box_cat, y=box_x, title="Box plot of " + box_x,
                         template="plotly_white", category_orders={"pos_simple": ["PG", "SG", "SF", "PF", "C"]})
 st.write(box_fig)
@@ -46,15 +49,19 @@ st.write(box_fig)
 st.header("Correlations")
 corr_x = st.selectbox("Correlation - X variable", options=df.columns, index=df.columns.get_loc("fg3a_per_g"))
 corr_y = st.selectbox("Correlation - Y variable", options=df.columns, index=df.columns.get_loc("efg_pct"))
-corr_filt = st.selectbox("Filter variable", options=df.columns, index=df.columns.get_loc("gs"))
-min_filt = st.number_input("Minimum value", value=40, min_value=0)
-fig = px.scatter(df[df[corr_filt] > min_filt], x=corr_x, y=corr_y, template="plotly_white",
-                 color="season", hover_data=['name', 'pos', 'age', 'season'], color_continuous_scale=px.colors.sequential.OrRd)
-fig.update_traces(mode="markers", marker={"line": {"width": 0.4, "color": "slategrey"}, "size": 2.5})
+corr_col = st.radio("Correlation - color variable", options=["age", "season", "pos_simple"], index=1)
+corr_filt = st.selectbox("Filter variable", options=df.columns, index=df.columns.get_loc("fg3a_per_g"))
+min_filt = st.number_input("Minimum value", value=6, min_value=0)
+tmp_df = df[df[corr_filt] > min_filt]
+fig = px.scatter(tmp_df, x=corr_x, y=corr_y, template="plotly_white", render_mode='webgl',
+                 color=corr_col, hover_data=['name', 'pos', 'age', 'season'], color_continuous_scale=px.colors.sequential.OrRd,
+                 category_orders={"pos_simple": ["PG", "SG", "SF", "PF", "C"]})
+fig.update_traces(mode="markers", marker={"line": {"width": 0.4, "color": "slategrey"}})
+st.subheader("Filtered scatterplot and dataframe")
 st.write(fig)
+st.write(tmp_df)
 
 # correlation heatmap
 hmap_params = st.multiselect("Select parameters to include on heatmap", options=list(df.columns), default=[p for p in df.columns if "fg" in p])
-# st.write(hmap_params)
 hmap_fig = px.imshow(df[hmap_params].corr())
 st.write(hmap_fig)
